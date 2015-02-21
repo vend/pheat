82ea7b20bbd4d5f94774ed27fde19cf9e9a483e6
Dominic Scheirlinck <dominic@vendhq.com>
2015-02-21 01:09:21 +0000

    Use PSR processor for nicer logs

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -6,7 +6,11 @@ $loader->addPsr4('Pheat\\', __DIR__);
 $build = __DIR__ . '/../build';
 
 $logger = new Monolog\Logger('test');
-$logger->pushHandler(new Monolog\Handler\StreamHandler($build . '/test.log'));
+
+$handler = new Monolog\Handler\StreamHandler($build . '/test.log');
+$handler->pushProcessor(new \Monolog\Processor\PsrLogMessageProcessor());
+
+$logger->pushHandler($handler);
 
 $settings = new Pheat\Test\Settings();
 $settings->setLogger($logger);
